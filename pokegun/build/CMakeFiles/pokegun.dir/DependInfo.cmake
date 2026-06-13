e68e0dec09b288cba38c6a36abef21f2ee98ef10
Anderb <anderskjacobsen@gmail.com>
2026-06-13 23:52:14 +0200

    Added folders to sort the program properly. Added CMakeList. Added Character class with the atribute name and the ability to store up to for monster and the corresponding getters and setters.

---
+++ pokegun/build/CMakeFiles/pokegun.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/anders/Desktop/Skole/Softwareudvikling/Portefølje_opgave/Softwareudvikling-portef-lje-opgave/pokegun/src/Character.cpp" "CMakeFiles/pokegun.dir/src/Character.cpp.o" "gcc" "CMakeFiles/pokegun.dir/src/Character.cpp.o.d"
+  "/home/anders/Desktop/Skole/Softwareudvikling/Portefølje_opgave/Softwareudvikling-portef-lje-opgave/pokegun/src/Monster.cpp" "CMakeFiles/pokegun.dir/src/Monster.cpp.o" "gcc" "CMakeFiles/pokegun.dir/src/Monster.cpp.o.d"
+  "/home/anders/Desktop/Skole/Softwareudvikling/Portefølje_opgave/Softwareudvikling-portef-lje-opgave/pokegun/src/main.cpp" "CMakeFiles/pokegun.dir/src/main.cpp.o" "gcc" "CMakeFiles/pokegun.dir/src/main.cpp.o.d"
+  "" "pokegun" "gcc" "CMakeFiles/pokegun.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
